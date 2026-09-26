287fef4bf8a2d771256a120aa9a4253d2fd1102a
vionaosiako <viona.osiako@student.moringaschool.com>
2026-09-26 12:06:40 +0300

    upadate appointment.php

--- appointment.php
+++ appointment.php
@@ -65,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Appointment - Sunrise Community Hospital</title>
+    <title>Book Appointment</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>

5cd95d8eba94b46d03fe8c37c458187bb658c24a
vionaosiako <viona.osiako@student.moringaschool.com>
2026-09-26 11:02:29 +0300

    Update Sunrise Hospital Management System

--- admin.php
+++ admin.php
@@ -41,7 +41,7 @@ if (!empty($search)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Appointment Records Management</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
